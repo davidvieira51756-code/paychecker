8852907d7571fc6d8c024f521684887443774889
davidvieira51756-code <davidvieira51756@gmail.com>
2026-04-27 16:13:52 +0100

    Initialize Spring Boot project with PostgreSQL and Flyway

---
+++ src/main/java/com/paychecker/PaycheckerApplication.java
@@ -0,0 +1,13 @@
+package com.paychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaycheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaycheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paychecker/PaycheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaycheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
